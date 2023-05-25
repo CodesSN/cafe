67d03c151009e4e1d2b1ddd6f68fc9c48d8510d2
Hibran <hibran.acosta@pidelectronics.com>
2023-05-25 14:13:56 -0600

    start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,49 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cafe';
+  inp1: any = 0;
+  inp2: any = 0;
+  inp3: any = 0;
+  cart: any[] = [{
+    tittle: 'cafe',
+    img: '../assets/images/product-1.png',
+    quantity: 10
+  },
+  {
+    tittle: 'cafe',
+    img: '../assets/images/product-1.png',
+    quantity: 10
+  },
+  {
+    tittle: 'cafe',
+    img: '../assets/images/product-1.png',
+    quantity: 10
+  }
+];
+  async addToCart(item:any,inp:any){
+    this.cart.push({
+      tittle: 'cafe fresco',
+      img: '../assets/images/product-'+ inp +'.png',
+      quantity: (inp === 1)? this.inp1 : (inp === 2)? this.inp2 : this.inp3,
+    })
+  }
+  async addValue(t: any) {
+    switch (t) {
+      case 1:
+        this.inp1++;
+        break;
+      case 2:
+        this.inp2++;
+        break;
+      case 3:
+        this.inp3++;
+        break;
+
+      default:
+        break;
+    }
+  }
 }
